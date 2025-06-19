2e8bd67eb40a84b822b9a6d1fa075123180cffbd
Tokarski <tokarski2011@gmail.com>
2025-06-18 23:57:29 -0300

    remove rust_analyzer format com save

--- init.lua
+++ init.lua
@@ -674,7 +674,11 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
